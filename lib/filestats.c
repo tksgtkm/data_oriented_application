b13a6794524f6a455ba26e3f8e0f7247a2e7812a
tksgtkm <h9cd342fd7jgx36@gmail.com>
2022-12-24 07:45:54 +0900

    filestats実装

--- lib/filestats.c
+++ lib/filestats.c
@@ -34,3 +34,42 @@ File *Fopen(char *name, char *mode, unsigned long magic) {
   F->Cumulative = F->Current;
   return F;
 }
+
+int Fclose(File *F) {
+  int num;
+  if (!F)
+    return 0;
+
+  num = fclose(F->f);
+  if (F->pathname)
+    Xfree(F->pathname);
+  Xfree(F);
+  return num;
+}
+
+size_t Fread(void *ptr, size_t size, size_t nitems, File *F) {
+  int num;
+  num = fread((char *) ptr, size, nitems, F->f);
+  F->Current.NumReads++;
+  F->Current.NumBytes += num * size;
+  return num;
+}
+
+int Fseek(File *F, long offset, int ptrname) {
+  int num;
+  num = fseek(F->f, offset, ptrname);
+  F->Current.NumSeeks++;
+  return num;
+}
+
+void Rewind(File *F) {
+  rewind(F->f);
+  F->Current.NumSeeks++;
+}
+
+void ZeroFileStats(File *F) {
+  F->Cumulative.NumSeeks += F->Current.NumSeeks;
+  F->Cumulative.NumReads += F->Current.NumReads;
+  F->Cumulative.NumBytes += F->Current.NumBytes;
+  F->Current.NumSeeks = F->Current.NumReads = F->Current.NumBytes = 0;
+}
